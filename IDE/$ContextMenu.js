9318dd99b3a5af7c79840f3ee991687fd18ac77f
Isaiah Odhner <isaiahodhner@gmail.com>
2014-02-21 15:10:54 -0500

    Clean up $ContextMenu.js
    It's better not to pass event objects when unnecessary.

--- IDE/$ContextMenu.js
+++ IDE/$ContextMenu.js
@@ -1,18 +1,20 @@
 (function($){
-	var $m;
-	var $ContextMenu = function(e,menuitems){
-		if($m)$m.remove();
+	var $old_menu;
+	var $ContextMenu = function(x, y, items){
+		if($old_menu){
+			$old_menu.remove();
+		}
 		var $menu = $("<ul class='context-menu'/>").appendTo("body");
 		$menu.css({
 			position: "absolute",
 			zIndex: 20000,
-			left: e.clientX,
-			top: e.clientY
+			left: x,
+			top: y
 		});
-		$(window).one("blur click mousedown",function(){
+		$(window).one("blur click mousedown", function(){
 			$menu.remove();
 		});
-		$.each(menuitems,function(i,item){
+		$.each(items, function(i, item){
 			var $item = $("<li class='context-menu-item'/>");
 			$item.css({
 				cursor: "default"
@@ -28,39 +30,27 @@
 			});
 			$menu.append($item);
 		});
-		return $m = $menu;
+		return $old_menu = $menu;
 	};
-	$.fn.contextMenu = function(menuitems){
-		this.on("contextmenu",function(e){
-			$ContextMenu(e,menuitems);
+	$.fn.contextMenu = function(items){
+		if(items === "clipboard"){
+			if(typeof process === "undefined"){
+				return;//only needed in node-webkit
+			}
+			items = [
+				{text:"Cut", click:function(){
+					document.execCommand("cut");
+				}},
+				{text:"Copy", click:function(){
+					document.execCommand("copy");
+				}},
+				{text:"Paste", click:function(){
+					document.execCommand("paste");
+				}},
+			];
+		}
+		this.on("contextmenu", function(e){
+			$ContextMenu(e.clientX, e.clientY, items);
 		});
 	};
-	$.fn.enableCopyPaste = function(menuitems){
-		if(typeof process === "undefined"){
-			return;//only needed in node-webkit
-		}
-		this.contextMenu([
-			{text:"Cut", click:function(){
-				document.execCommand("cut");
-			}},
-			{text:"Copy", click:function(){
-				document.execCommand("copy");
-			}},
-			{text:"Paste", click:function(){
-				document.execCommand("paste");
-			}},
-		]);
-		/*this.contextMenu([
-			{text: "Cut", click: function(){
-				//?
-			}},
-			{text: "Copy", click: function(){
-				
-			}},
-			{text: "Paste", click:function(){
-				 
-			}},
-		]);*/
-	};
-	
 })(jQuery);
